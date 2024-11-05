0334b1ae43fee3c1ddd459c1da48bf9e5abe0bab
Tyler Fang <tyler.fang04@gmail.com>
2024-11-05 18:56:03 -0500

    roster list

---
+++ roster.py
@@ -0,0 +1,4 @@
+# https://goheels.com/sports/mens-basketball/roster
+
+roster = [ "Cadeau", "Davis", "Tyson", "Trimble", "Powell", "Jackson", "Washington", "Withers", "Claude", "Brown"]
+print(roster)
