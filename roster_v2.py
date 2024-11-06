e34b6408582c588f42e98531af92701f1184624b
Tyler Fang <tyler.fang04@gmail.com>
2024-11-05 20:03:27 -0500

    0 header

---
+++ roster_v2.py
@@ -0,0 +1,7 @@
+#https://goheels.com/sports/mens-basketball/roster
+
+import pandas as pd
+
+roster = ["Cadeau", "Davis", "Tyson", "Trimble", "Powell", "Jackson", "Washington", "Withers", "Claude", "Brown"]
+data = pd.DataFrame(roster)
+print(data)
